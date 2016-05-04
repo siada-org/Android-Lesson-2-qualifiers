847c67a02cf64db07099d51f414260e1bd35801c
Kirill Silianov <admin@siada.org>
2016-05-04 17:54:18 +0600

    Stage1
    - добавлены файлы ресурсов размеров для sw600, sw720, sw1280
    - добавлены строковые ресурсы названий кнопок для портретной и
    ландшафтной ориентации экрана
    - создан класс запускающий начальный экран
    ##Протестировано

---
+++ app/src/main/java/org/siada/androidlesson2qualifiers/OrientationActivity.java
@@ -0,0 +1,15 @@
+package org.siada.androidlesson2qualifiers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Admin on 04.05.2016.
+ */
+public class OrientationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.orientation_activity);
+    }
+}
